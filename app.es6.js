939d0487675d20ae1cf5fc95e3c44c489a93321c
Chanaka Sandaruwan <chanakasan@gmail.com>
2016-03-06 14:16:58 +0530

    initial

---
+++ app.es6.js
@@ -0,0 +1,13 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  greet() {
+    return `Hello I am ${this.name}`;
+  }
+}
+
+let person = new Person("Chanaka");
+console.log(person.greet());
+
